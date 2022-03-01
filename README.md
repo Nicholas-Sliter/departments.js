5257073fe792a2750eb8972a73102fb5d45d1bb5
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-03-01 08:19:38 -0500

    Copy existing files from directory.js as templete

---
+++ README.md
@@ -0,0 +1,25 @@
+# departments.js
+JavasScript / TypeScript API for the Middlebury Directory at directory.middlebury.edu/default.aspx.
+Node package at https://www.npmjs.com/package/directory.js.
+
+Use it like this
+```js
+
+import { Scraper } from 'directory.js';
+
+const middleburyEmail = "example@middlebury.edu";
+const S = new Scraper(middleburyEmail);
+
+async () => {
+  await S.init();
+  console.log(S.person);
+
+}
+
+
+
+
+
+
+```
+
---
+++ src/scraper.ts
@@ -0,0 +1,65 @@
+import fetch from "node-fetch";
+import { parse } from "node-html-parser";
+import Person from "./person.js";
+import { viewstate, eventvalidation } from "./constants.js";
+
+export const DIRECTORY_URL = "https://directory.middlebury.edu/";
+export const SEARCH_URL = "https://directory.middlebury.edu/default.aspx";
+export const PERSON_URL = "https://directory.middlebury.edu/GetRecord.aspx";
+
+export class Scraper {
+  email?: string;
+  person: Person;
+  id?: string;
+
+  constructor(email: string = null, id: string = null) {
+    this.email = email ?? null; 
+    this.id = id ?? null;
+  }
+
+  public async init(): Promise<void> {
+    if (!this.id) {
+    this.id = await this.getIDByEmail(this.email);
+    }
+    this.person = new Person(this.id);
+    await this.person.init();
+
+  }
+
+  private async getIDByEmail(email:string): Promise<string> {
+
+    const urlEncoded = new URLSearchParams();
+
+    urlEncoded.append("__VIEWSTATE", viewstate);
+    urlEncoded.append("__EVENTVALIDATION", eventvalidation);
+    urlEncoded.append(
+      "ctl00$ctl00$PageContent$PageContent$middDirectoryForm$txtSamaccountname",
+      email
+    );
+    urlEncoded.append(
+      "ctl00$ctl00$PageContent$PageContent$middDirectoryForm$btnSearch",
+      "Search"
+    );
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: "ASP.NET_SessionId=orauz5z5ytfp3ebtz0r3eehv",
+      },
+      body: urlEncoded,
+    };
+
+    const res = await fetch(
+      "https://directory.middlebury.edu/",
+      requestOptions
+    );
+    const text = await res.text();
+    const root = parse(text);
+    const link = root.querySelector(
+      "#ctl00_ctl00_PageContent_PageContent_middDirectoryForm_lstResults_ctl02_lnkName"
+    ).attributes.href;
+
+    return link.split("#")[1];
+  }
+}
