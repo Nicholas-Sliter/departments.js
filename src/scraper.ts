c9c5552f7f7d6026c71c8af45d1c1b077d34909f
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-03-02 15:01:45 -0500

    Remove person template cde

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,11 +1,9 @@
 import fetch from "node-fetch";
 import { parse } from "node-html-parser";
-import Person from "./person.js";
 import Department from "./department.js"
 
 export const DIRECTORY_URL = "https://directory.middlebury.edu/";
-export const SEARCH_URL = "https://directory.middlebury.edu/default.aspx";
-export const PERSON_URL = "https://directory.middlebury.edu/GetRecord.aspx";
+
 
 export class DepartmentScraper{
   code: string;
@@ -13,62 +11,5 @@ export class DepartmentScraper{
 
   static async getDepartments(season: string = "F"): Promise<Department[]> {
       return Department.getDepartments(season);
-  }
-}
-
-export class PersonScraper {
-  email?: string;
-  person: Person;
-  id?: string;
-
-  constructor(email: string = null, id: string = null) {
-    this.email = email ?? null; 
-    this.id = id ?? null;
-  }
-
-  public async init(): Promise<void> {
-    if (!this.id) {
-    this.id = await this.getIDByEmail(this.email);
-    }
-    this.person = new Person(this.id);
-    await this.person.init();
-
-  }
-
-  private async getIDByEmail(email:string): Promise<string> {
-
-    const urlEncoded = new URLSearchParams();
-
-    // urlEncoded.append("__VIEWSTATE", viewstate);
-    // urlEncoded.append("__EVENTVALIDATION", eventvalidation);
-    urlEncoded.append(
-      "ctl00$ctl00$PageContent$PageContent$middDirectoryForm$txtSamaccountname",
-      email
-    );
-    urlEncoded.append(
-      "ctl00$ctl00$PageContent$PageContent$middDirectoryForm$btnSearch",
-      "Search"
-    );
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Cookie: "ASP.NET_SessionId=orauz5z5ytfp3ebtz0r3eehv",
-      },
-      body: urlEncoded,
-    };
-
-    const res = await fetch(
-      "https://directory.middlebury.edu/",
-      requestOptions
-    );
-    const text = await res.text();
-    const root = parse(text);
-    const link = root.querySelector(
-      "#ctl00_ctl00_PageContent_PageContent_middDirectoryForm_lstResults_ctl02_lnkName"
-    ).attributes.href;
-
-    return link.split("#")[1];
   }
 }
