5972bf02a44924c766ff1c44c4be0f3ee4e72f62
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-03-02 15:14:17 -0500

    Basic structure

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,15 +1,28 @@
 import fetch from "node-fetch";
 import { parse } from "node-html-parser";
 import Department from "./department.js"
+import {getDepartmentsURL} from "./utils.js"
 
-export const DIRECTORY_URL = "https://directory.middlebury.edu/";
 
-
-export class DepartmentScraper{
-  code: string;
-  name: string;
-
-  static async getDepartments(season: string = "F"): Promise<Department[]> {
-      return Department.getDepartments(season);
+export default class Scraper {
+  season: string;
+  departments: Department[] = [];
+  
+  constructor(season:string) {
+    this.season = season ?? null;
+  }
+  
+  public async init(){
+    this.departments = this.getDepartments(this.season); 
+  }
+  
+  static async getDepartments(season: string): Promise<Department[]> {
+       const departments: Department[] = [];
+    
+      //do fetch here and split the list the departments class then parse the html for each department
+    
+    
+   
+     return departments;
   }
 }
